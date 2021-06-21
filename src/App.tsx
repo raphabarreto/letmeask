0328a1caae409b3b16692b1e50dc36f44713c851
Raphael Barreto <tnt.raphael@gmail.com>
2021-06-21 11:11:11 -0300

    Integração com Firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Button } from './components/Button';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button title="Botão 1" />
+      <Button title="Botão 2" />
+      <Button />
+    </>
+  );
 }
 
 export default App;
